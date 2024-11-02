1e67d516553c4d67f2c704f9f8a78529b6150783
Emily Culp <eculp@ycp.edu>
2024-11-02 18:30:51 -0400

    solder

--- rfid/rfid.ino
+++ rfid/rfid.ino
@@ -1,7 +1,8 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <SoftwareSerial.h>
 
-#define SS_PIN 10
+#define SS_PIN 53
 #define RST_PIN 5
 
 MFRC522 rfid(SS_PIN, RST_PIN);
@@ -9,15 +10,26 @@ MFRC522 rfid(SS_PIN, RST_PIN);
 bool isDocked = false;
 unsigned long lastDetectedMillis = 0;
 const long dockedDuration = 2000; // Duration for docked state
+// SoftwareSerial espSerial(18, 19);
 
 void setup() {
   Serial.begin(9600);
+  Serial1.begin(9600);  //communication with esp8266
+  // espSerial.begin(9600);
+
   SPI.begin();
   rfid.PCD_Init();
   Serial.println("RFID reader initialized. Tap card on reader.");
+
+  initializeESP();
 }
 
 void loop() {
+  if (Serial1.available()) {
+      char c = Serial1.read();
+      Serial.print(c);  // Print to the Serial Monitor
+  }
+
   unsigned long currentMillis = millis();
 
   // Check if a new card is present
@@ -38,6 +50,7 @@ void loop() {
   } else {
     // If the card is not new and it was docked, check for removal
     if (isDocked) {
+      Serial.println("is present and not new isDocked");
       // Check if the docked duration has passed
       if (currentMillis - lastDetectedMillis > dockedDuration) {
         Serial.println("Card removed, stopping UID print.");
@@ -51,6 +64,7 @@ void loop() {
     }
   }
 
+  checkForClientRequest();
   delay(1000); // Control the loop frequency
 }
 
@@ -60,4 +74,36 @@ void printUID() {
     Serial.print(rfid.uid.uidByte[i], HEX);
   }
   Serial.println();
+}
+
+// Initialize ESP8266
+void initializeESP(){
+  Serial1.println("AT+RST"); // Reset ESP8266
+  delay(2000);
+  Serial1.println("AT+CWMODE=2"); // Set to access point mode
+  delay(1000);
+  Serial1.println("AT+CIFSR"); // Check IP address
+  delay(1000);
+  Serial1.println("AT+CIPMUX=1"); // Enable multiple connections
+  delay(1000);
+  Serial1.println("AT+CIPSERVER=1,80"); // Start server on port 80
+  delay(1000);
+}
+
+void checkForClientRequest() {
+  if (Serial1.available()) {
+    String request = Serial1.readStringUntil('\r');
+    if (request.indexOf("GET / ") >= 0) {
+      sendHttpResponse(isDocked ? "Docked" : "Not Docked");
+    }
+  }
+}
+
+// Function to send HTTP response
+void sendHttpResponse(String message) {
+  Serial1.println("AT+CIPSEND=0," + String(message.length() + 4));
+  delay(100);
+  Serial1.println(message);
+  delay(100);
+  Serial1.println("AT+CIPCLOSE=0");
 }
